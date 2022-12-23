a93d90a6a3515eb42fcdf1d0662f478a7d59b6aa
Julien <48369040+julien040@users.noreply.github.com>
2022-12-23 15:30:09 +0100

    :construction: Add clone cmd check if directory is empty

--- src/prompt/prompt.go
+++ src/prompt/prompt.go
@@ -21,6 +21,9 @@ func InputLine(message string) (string, error) {
 	return strings.TrimSuffix(input, "\n"), nil
 }
 
+// Prompt the user for a string input and validate it
+// If the input is not valid, the error message is displayed and the user is prompted again
+// The validation function should return true if the input is valid
 func InputWithValidation(message string, errorMessage string, validation func(string) bool) (string, error) {
 	valid := false
 	var res string
@@ -39,13 +42,25 @@ func InputWithValidation(message string, errorMessage string, validation func(st
 	return res, nil
 }
 
-func InputBool(message string) (bool, error) {
-	res, err := InputWithValidation(message+" (Y/n)", "\nPlease enter either y or n",
+// InputBool prompts the user for a boolean input and returns it
+//
+// The default value is used if the user doesn't enter anything
+// The message should not contain the "(y/n)" part
+func InputBool(message string, defaultValue bool) (bool, error) {
+	if defaultValue {
+		message += " (Y/n): "
+	} else {
+		message += " (y/N): "
+	}
+	res, err := InputWithValidation(message, "\nPlease enter either y or n",
 		func(str string) bool {
 			return str == "y" || str == "n" || str == "Y" || str == "N" || str == ""
 		})
 	if err != nil {
 		return false, err
 	}
-	return res == "y" || res == "Y" || res == "", nil
+	if res == "" {
+		return defaultValue, nil
+	}
+	return res == "y" || res == "Y", nil
 }
