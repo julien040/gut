7eaccf5bf429a093c86f6b48eac39266ec8e1184
Julien <48369040+julien040@users.noreply.github.com>
2022-12-27 12:15:38 +0100

    :bug: Fix : gut save doesn't respect .gitignore Because of an issue in go-git, commiting was ignoring .gitignore. I had to parse the .gitignore and fill the .exclude property of Worktree. Now, .gitignore is taken in account. But nested .gitignore are ignored

--- src/executor/commit.go
+++ src/executor/commit.go
@@ -1,7 +1,12 @@
 package executor
 
 import (
+	"bufio"
+	"os"
+	"path/filepath"
+
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/format/gitignore"
 )
 
 type CommitResult struct {
@@ -20,6 +25,10 @@ func Commit(path string, message string) (CommitResult, error) {
 	if err != nil {
 		return CommitResult{}, err
 	}
+	// Replace the .gitignore file with the one in the repo
+	replaceGitIgnore(w, filepath.Join(path, ".gitignore"))
+
+	// Add all files
 	err = w.AddWithOptions(&git.AddOptions{
 		All: true,
 	})
@@ -35,9 +44,6 @@ func Commit(path string, message string) (CommitResult, error) {
 	fileUpdated := 0
 	fileDeleted := 0
 	for _, v := range status {
-		if v.Worktree == git.Unmodified {
-			continue
-		}
 		if v.Staging == git.Added {
 			fileAdded++
 		}
@@ -60,4 +66,22 @@ func Commit(path string, message string) (CommitResult, error) {
 		FilesUpdated: fileUpdated,
 		FilesDeleted: fileDeleted,
 	}, nil
+}
+
+// Load the .gitignore file, and add to the "exclude" section of the worktree
+//
+// This is a workaround for the fact that go-git does not support .gitignore
+func replaceGitIgnore(wk *git.Worktree, path string) {
+	f, _ := os.Open(path)
+	defer f.Close()
+
+	// Read the file
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Add the line to the exclude list
+		wk.Excludes = append(wk.Excludes, gitignore.ParsePattern(line, nil))
+	}
+
 }
