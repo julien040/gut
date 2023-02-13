e9944797f416f2defcce2f799188539881c14e6d
julien040 <48369040+julien040@users.noreply.github.com>
2023-02-13 17:00:09 +0100

    📈 Add anonymous telemetry If telemetry settings is not set, we should ask the user if he wants to enable it. To do so, we run a function called promptForTelemetry before executing the command in root.go The telemetry is anonymous, it gets the command called, the CPU Cores, the OS, the arch and the version of CLI Any function should now call LogCommand at its beginning to log the event To send the event to the server without blocking the running of the command, we use a goroutine. However, to ensure that the request is sent before closing the CLI, we use a WaitGroup. It defers closing only when sendEvent is done

--- cmd/root.go
+++ cmd/root.go
@@ -24,6 +24,8 @@ package cmd
 import (
 	"os"
 
+	"github.com/julien040/gut/src/prompt"
+	"github.com/julien040/gut/src/telemetry"
 	"github.com/spf13/cobra"
 )
 
@@ -43,10 +45,27 @@ Our goal is not to replace git, but to make it easier to use for the most common
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
+	// Prompt for telemetry consent
+	promptForTelemetry()
+
+	// Run the command called by the user
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func promptForTelemetry() {
+	consentStateKnown := telemetry.IsConsentStateKnown()
+	if !consentStateKnown {
+		res, err := prompt.InputBool("Do you want to help us improve gut by sending anonymous usage data?", true)
+		if err != nil {
+			return
+		}
+		telemetry.SetConsentState(res)
+	}
+
 }
 
 func init() {
