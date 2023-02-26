c46e6a291e66df352d65b38312d4deb583c6a1a7
julien040 <48369040+julien040@users.noreply.github.com>
2023-02-26 20:01:25 +0100

    🐛 Fix ref not found on init repo If profile initialisation fails on a new repo, a repo would be initialised. However, no commit would be made. This lead to issue when gut tries to figured out the HEAD. It couldn't because there isn't any commits.
    To solve this issue, initialisation of a git repo is now made after the profile selection. If selection fails, it exits.
    Therefore, the repo won't be left in a state initialised but without any commits

--- src/controller/init.go
+++ src/controller/init.go
@@ -13,6 +13,10 @@ func Init(cmd *cobra.Command, args []string) {
 	if err != nil {
 		exitOnError("Oups, something went wrong while getting the current working directory", err)
 	}
+	// Profile selection is done before the git init to avoid an empty repository
+	// If the profile selection fails, we don't want a git repo without any commits
+	// Gut wouldn't be able to find the HEAD commit
+	profile := selectProfile("", true)
 	if executor.IsPathGitRepo(wd) {
 		exitOnError("Oups, this directory is already a git repository. Delete the .git folder if you want to initialize a new repository", nil)
 	}
@@ -20,7 +24,7 @@ func Init(cmd *cobra.Command, args []string) {
 	if err != nil {
 		exitOnError("Oups, something went wrong while initializing the repository", err)
 	}
-	profile := selectProfile("", true)
+
 	associateProfileToPath(profile, wd)
 	_, err = executor.Commit(wd, "🎉 Initial commit from Gut")
 	if err != nil {
