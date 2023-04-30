6dccd5f8c95ca696c1564f27d8000c223e69b7f4
Julien C <48369040+julien040@users.noreply.github.com>
2023-04-30 17:46:13 +0200

    ✨Add new capabilites to switch cmd (Fix #47)
    * 🚸 Add keep changes when switching branches
    Before, when the working tree wasn't clean, switching branch discarded changes
    To avoid this behaviour, gut shows two options :
    - Discard changes as before
    - Keep the changes
    
    To do the later, gut stash changes, switch the branch and stash pop so that the user can commit modified files
    
    * ✨ Add switch to commit with gut switch
    To get closer to Git switch behaviour, `gut switch` can now switch to a commit
    First, it checks if the arg is a branch. If not, it checks if it's a commit. Finally, if it's not a commit, gut prompts to create a new branch called from the arg
    
    Also, I've fixed a bug where gut was switching to the current branch. It's because there wasn't any check to verify the user wasn't trying to checkout the same branch.

--- src/controller/switch.go
+++ src/controller/switch.go
@@ -21,34 +21,64 @@ func Switch(cmd *cobra.Command, args []string) {
 	checkIfGitRepoInitialized(wd)
 
 	// Input the branch name
-	var branchName string
+	var refArg string
 	if len(args) == 0 {
-		res, err := prompt.InputLine("Switch to branch: ")
+		res, err := prompt.InputLine("Switch to: ")
 		if err != nil {
 			exitOnKnownError(errorReadInput, err)
 		}
-		branchName = res
+		refArg = res
 	} else {
-		branchName = args[0]
+		refArg = args[0]
 	}
 
 	// Define a reusable spinner
 	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
 
 	// Check if the branch exists
-	exists, err := executor.CheckIfBranchExists(wd, branchName)
+	exists, err := executor.CheckIfBranchExists(wd, refArg)
 	if err != nil {
 		exitOnError("I can't check if the branch exists", err)
 	}
 
-	// If the branch doesn't exist, ask the user if he wants to create it
+	// If the branch doesn't exist, we check if the user wants to switch to a commit
 	if !exists {
+		// Check if the user wants to switch to a commit
+		commit, err := executor.GetCommitByHash(wd, refArg)
+		// If the commit exists, we switch to it
+		if err == nil {
+
+			// Check if the working tree is clean
+			s.Prefix = "Checking if the working tree is clean... "
+			s.Start()
+			clean, err := executor.IsWorkTreeClean(wd)
+			s.Stop()
+			if err != nil {
+				exitOnError("I can't check if there are uncommitted changes", err)
+			}
+			// If not clean, we return an error
+			if !clean {
+				exitOnError("Sorry, you have uncommitted changes. Save them with \"gut save\" before going to another commit or you will lose them", nil)
+			}
+
+			// Switch to the commit
+			s.Prefix = "Switching to the commit... "
+			s.Start()
+			err = executor.CheckoutCommit(wd, commit.Hash.String())
+			s.Stop()
+			if err != nil {
+				exitOnError("I can't switch to the commit", err)
+			}
+			print.Message("I've successfully switched to the commit %s", print.Info, commit.Hash.String())
+			return
+		}
+
 		res, err := prompt.InputBool("Uh oh, the branch doesn't exist. Do you want me to create it?", true)
 		if err != nil {
 			exitOnKnownError(errorReadInput, err)
 		}
 		if res {
-			err = executor.CreateBranch(wd, branchName)
+			err = executor.CreateBranch(wd, refArg)
 			if err != nil {
 				exitOnError("My bad, I can't create the branch", err)
 			}
@@ -58,8 +88,23 @@ func Switch(cmd *cobra.Command, args []string) {
 		}
 	} else { // If the branch exists, switch to it
 
+		// Check if the branch is the current branch
+		currentBranch, err := executor.GetCurrentBranch(wd)
+		if err != nil {
+			exitOnError("I can't get the current branch", err)
+		}
+		if currentBranch == refArg {
+			print.Message("You are already on the branch "+refArg, print.Success)
+			return
+		}
+
+		// Check if the working tree is clean
 		s.Prefix = "Checking if the working tree is clean... "
 		s.Start()
+
+		// Set to true when a stash is created
+		// We need to pop the stash at the end of the function
+		mustStashPop := false
 
 		// Check if the working tree is clean
 		clean, err := executor.IsWorkTreeClean(wd)
@@ -69,24 +114,42 @@ func Switch(cmd *cobra.Command, args []string) {
 		}
 		// If not clean, ask the user if he wants to continue because the changes might be lost
 		if !clean {
-			res, err := prompt.InputBool("Uh oh, there are uncommitted changes. They might be lost if you switch branches. Do you want to continue?", false)
+			/*
+				Because the working tree is not clean, we have two options:
+					- stash the changes
+					- discard the changes
+
+				We will ask the user which option he wants to use
+			*/
+			print.Message("Uh oh, there are uncommitted changes", print.Warning)
+
+			res, err := prompt.InputSelect("What do you want to do?", []string{"Keep the changes", "Discard the changes"})
 			if err != nil {
 				exitOnKnownError(errorReadInput, err)
 			}
-			if !res {
-				print.Message("Okay, I won't switch branches", print.Info)
-				return
+
+			// If he wants to keep the changes, we stash them
+			// Also, we set a flag so that we can pop the stash at the end of the function
+			if res == "Keep the changes" {
+				executor.GitStash()
+				mustStashPop = true
 			}
 		}
-		s.Prefix = "Switching to the branch " + branchName + " "
+		s.Prefix = "Switching to the branch " + refArg + " "
 		s.Start()
-		err = executor.CheckoutBranch(wd, branchName)
+
+		err = executor.CheckoutBranch(wd, refArg)
+
+		// If we stashed the changes, we need to pop the stash
+		if mustStashPop {
+			executor.GitStashPop()
+		}
 		s.Stop()
 		if err != nil {
-			exitOnError("I can't switch to the branch "+branchName, err)
+			exitOnError("I can't switch to the branch "+refArg, err)
 		}
 
 	}
-	print.Message(`I switched to the branch "`+branchName+`" successfully 🎉`, print.Success)
+	print.Message(`I switched to the branch "`+refArg+`" successfully 🎉`, print.Success)
 
 }
