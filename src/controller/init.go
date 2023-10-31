e84e84d4711492c3a40b43882f1db3952e18a71e
julien040 <48369040+julien040@users.noreply.github.com>
2023-10-31 17:51:25 +0100

    ✨ init cmd now adds a .gitignore
    For better UX, `gut init` prompts the user if he wants to download a .gitignore template.

--- src/controller/init.go
+++ src/controller/init.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/julien040/gut/src/executor"
 	"github.com/julien040/gut/src/print"
+	"github.com/julien040/gut/src/prompt"
 	"github.com/spf13/cobra"
 )
 
@@ -23,6 +25,23 @@ func Init(cmd *cobra.Command, args []string) {
 	err = executor.Init(wd)
 	if err != nil {
 		exitOnError("Oups, something went wrong while initializing the repository", err)
+	}
+
+	// Ask for a .gitignore
+	res, err := prompt.InputBool("Do you want to create a .gitignore file?", true)
+	if err != nil {
+		exitOnKnownError(errorReadInput, err)
+	}
+	if res {
+		getGitignoreContentFromPath(wd)
+		templates, err := fetchIgnoreList()
+		if err != nil {
+			exitOnError("Sorry, I couldn't fetch the list of available gitignore templates 😓", err)
+		}
+		template := selectGitignoreTemplate(templates)
+		gitignoreTemplateContent := splitStringByNewLine(template.contents)
+		gitignoreTemplateContent = append(gitignoreTemplateContent, ".gut")
+		appendToGitignore(filepath.Join(wd, ".gitignore"), gitignoreTemplateContent, template.name)
 	}
 
 	associateProfileToPath(profile, wd)
